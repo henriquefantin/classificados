09f333fe855ad05e02ccd55cc6292c2f6834081b
henriqueFantin <henrique.fantin.ns@gmail.com>
2024-01-13 14:53:17 -0300

    modified:   resources/views/cadastros/formaPagamento.blade.php 	modified:   resources/views/cadastros/tipoAnuncio.blade.php

--- resources/views/cadastros/formaPagamento.blade.php
+++ resources/views/cadastros/formaPagamento.blade.php
@@ -12,12 +12,12 @@
                     <form action="" method="post">
                         @csrf
                         <div class="mb-4">
-                            <label for="descricao" class="form-label">Tipo do Pagamento</label>
-                            <input type="text" id="descricao" name="descricao" class="" placeholder="Descrição do tipo de pagamento Ex: Credito, boleto, pix">
+                            <label for="descricao" class="block text-sm font-medium leading-6 text-gray-900">Tipo do Pagamento</label>
+                            <input type="text" id="descricao" name="descricao" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Descrição do tipo de pagamento Ex: Credito, boleto, pix">
                         </div>
                         <div class="mb-4">
-                            <label for="parcelas" class="form-label">Limite de parcelas</label>
-                            <input type="number" id="parcelas" name="parcelas" class="">
+                            <label for="parcelas" class="block text-sm font-medium leading-6 text-gray-900">Limite de parcelas</label>
+                            <input type="number" id="parcelas" name="parcelas" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                         <div class="mt-6 flex items-center justify-end">
                             <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Salvar</button>
